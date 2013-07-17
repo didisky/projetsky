e6a668338ef6b2fe1e013bd9c1c3931ac057adde
Didi <Didi@Didi-PC>
2013-07-17 22:29:54 +0200

    mon premier commit

---
+++ projectSky/src/com/example/projectsky/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.projectsky;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		TextView coucou = null;
+		super.onCreate(savedInstanceState);
+		coucou = new TextView(this);
+		coucou.setText("hello didi");
+		setContentView(coucou);
+	}
+
+
+
+}
