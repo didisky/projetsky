93a10a96185462a737fea5af1e7f4bee4e7732e7
Didi <Didi@Didi-PC>
2013-07-17 23:35:54 +0200

    test du commit 2

--- projectSky/src/com/example/projectsky/MainActivity.java
+++ projectSky/src/com/example/projectsky/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 		TextView coucou = null;
 		super.onCreate(savedInstanceState);
 		coucou = new TextView(this);
-		coucou.setText("hello didi test numero 3");
+		coucou.setText("hello didi test numero 4");
 		setContentView(coucou);
 	}
 
